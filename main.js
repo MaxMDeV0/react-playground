6f7e4ef861b132bc338534a7e787748f7812b513
MaxM <119800989+MaxMDeV0@users.noreply.github.com>
2023-11-07 16:19:22 +0100

    update

--- main.js
+++ main.js
@@ -34,4 +34,71 @@ function UserCard({user}) {
     )
 }
 
+function NameForm (){
+    const [value, setValue] = useState('')
+
+    const handleChange = (e)=>{
+        setValue(e.target.value)
+    }
+    const handleSumbit = (e)=>{
+        alert('Le nom a été soumis : ' + value)
+        e.preventDefault();
+
+    }
+    return <form onSubmit={handleSumbit}>
+        <label>
+            Nom:
+            <input type='text' value={value} onChange={handleChange} />
+        </label>
+        <input type='submit' value='Envoyer' />
+    </form>
+}
+
+
+function EssayForm (){
+    const [value, setValue] = useState('Écrivez un essai à propos de votre élément du DOM préféré')
+
+    const handleChange = (e)=>{
+        setValue(e.target.value)
+    }
+    const handleSumbit = (e)=>{
+        alert('Un essai a été envoyé : ' + value)
+        e.preventDefault();
+
+    }
+    return <form onSubmit={handleSumbit}>
+        <label>
+            Essay:
+            <input type='text' value={value} onChange={handleChange} />
+        </label>
+        <input type='submit' value='Envoyer' />
+    </form>
+}
+function FlavorForm (){
+    const [value, setValue] = useState('coconut')
+
+    const handleChange = (e)=>{
+        setValue(e.target.value)
+    }
+    const handleSumbit = (e)=>{
+        alert('Votre parfum favori est : ' + value)
+        e.preventDefault();
+
+    }
+    return <form onSubmit={handleSumbit}>
+        <label>
+          Choisissez votre parfum favori :
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Pamplemousse</option>
+            <option value="lime">Citron vert</option>
+            <option value="coconut">Noix de coco</option>
+            <option value="mango">Mangue</option>
+          </select>
+        </label>       
+        <input type='submit' value='Envoyer' />
+    </form>
+}
+
+
+
 ReactDOM.render(<App/>, document.querySelector('#app'))
